b5a5bfa56b22b39e655e15b16106cb75381ff9eb
Thanh Uong <thanh@avatarnewyork.com>
2013-09-19 13:19:17 -0400

    Implemented lazy loading effect to posts

---
+++ assets/themes/twitter/js/custom.js
@@ -0,0 +1,26 @@
+$(function() {
+	$('#search-query').lunrSearch({
+		indexUrl: '/search.json',             // URL of the `search.json` index data for your site
+		results:  '#search-results',          // jQuery selector for the search results container
+		entries:  '.entries',                 // jQuery selector for the element to contain the results list, must be a child of the results element above.
+		template: '#search-results-template'  // jQuery selector for the Mustache.js template
+	});
+});
+
+
+
+$(document).ready(function(){
+	if ($('.previous .disabled') && $('.next .disabled')) {
+		$('#bottom-span').hide();
+	}
+
+    $(".waypoints-container").waypoint({
+        handler: function (direction){
+       		if (direction === "down"){
+            	$(this).delay(500).animate({opacity: 1});
+          	}
+        },
+        offset: '100%'
+    });
+
+});
